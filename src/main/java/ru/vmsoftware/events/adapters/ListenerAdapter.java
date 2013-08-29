c9363cf089fb58a45f0e1b38d0c762498663779b
acc15 <vyacheslav.mayorov@gmail.com>
2013-08-29 23:42:56 +0400

    SimpleAdapter renamed to ListenerAdapter

--- src/main/java/ru/vmsoftware/events/adapters/ListenerAdapter.java
+++ src/main/java/ru/vmsoftware/events/adapters/ListenerAdapter.java
@@ -6,7 +6,7 @@ import ru.vmsoftware.events.EventListener;
  * @author Vyacheslav Mayorov
  * @since 2013-30-04
  */
-public abstract class SimpleAdapter<E, T, D> implements EventListener<E, T, D> {
+public abstract class ListenerAdapter<E, T, D> implements EventListener<E, T, D> {
 
     public boolean isCounterpart(Object obj) {
         return equals(obj);
