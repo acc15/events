3badbcad28b101ae2248783221c04d87405e6d91
acc15 <vyacheslav.mayorov@gmail.com>
2013-09-28 17:53:01 +0400

    Tiny javadoc

--- src/main/java/ru/vmsoftware/events/providers/Providers.java
+++ src/main/java/ru/vmsoftware/events/providers/Providers.java
@@ -9,14 +9,35 @@ import java.lang.ref.WeakReference;
  */
 public class Providers {
 
+    // to prevent instantiation
+    private Providers() {}
+
+    /**
+     * Creates provider which provides object which holds given {@link Reference}
+     * @param reference reference to provide
+     * @param <T> type of {@link Provider}
+     * @return weak reference provider which provides given {@code obj}
+     */
     public static <T> Provider<T> ref(Reference<T> reference) {
         return new ReferenceProvider<T>(reference);
     }
 
+    /**
+     * Creates provider which provides specified object and holds him by {@link WeakReference}
+     * @param obj object to provide
+     * @param <T> type of {@link Provider}
+     * @return weak reference provider which provides given {@code obj}
+     */
     public static <T> Provider<T> weakRef(T obj) {
         return ref(new WeakReference<T>(obj));
     }
 
+    /**
+     * Creates provider which provides specified object and holds him by strong reference
+     * @param obj object to provide
+     * @param <T> type of {@link Provider}
+     * @return strong reference provider which provides given {@code obj}
+     */
     public static <T> Provider<T> strongRef(T obj) {
         return new StrongProvider<T>(obj);
     }
